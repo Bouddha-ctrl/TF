373f4d7e6ab7e5ca7f43b483c1661d7cb8ed54e9
Billy <kerimleyya25>
2022-09-23 14:49:42 +0200

    correction du premier

--- src/main/java/com/ufr/croissantshow/CroissantshowApplication.java
+++ src/main/java/com/ufr/croissantshow/CroissantshowApplication.java
@@ -2,8 +2,9 @@ package com.ufr.croissantshow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class CroissantshowApplication {
 
 	public static void main(String[] args) {
