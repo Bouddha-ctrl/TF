20fc5078993aa6f44b54d879de5c60fe8e713bc2
Billy <kerimleyya25>
2022-09-23 14:39:29 +0200

    arborescence projet

---
+++ src/main/java/com/ufr/croissantshow/CroissantshowApplication.java
@@ -0,0 +1,13 @@
+package com.ufr.croissantshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroissantshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroissantshowApplication.class, args);
+	}
+
+}
